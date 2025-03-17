403e4791ad3b87863e2f83fa796fd5a013008ced
Vitalii <vitalijborisenko27@gmail.com>
2025-03-17 12:52:22 +0200

    Practichna 3, Convert in console.

--- src/homework/hw01.rs
+++ src/homework/hw01.rs
@@ -1,4 +1,36 @@
 #[test]
-fn test1() {
-    assert_eq!(1 + 1, 2);
+fn main(){
+    const width: u32 = 25;
+    const height: u32 = 25;
+
+    for y in 0..height {
+        for x in 0..width{
+            if((y == 0 || y == height-1)
+                || (x == 0 || x == width-1)
+                || (x == y || x == height-y)){
+                print!("*");
+            }
+            else{
+                print!(" ");
+            }
+        }
+        println!()
+    }
+}
+#[test]
+fn clear_code_competition(){
+    const WIDTH: u32 = 25;
+    const HEIGHT: u32 = 25;
+
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
+            let is_horizontal: bool = (y == 0 || y == HEIGHT-1);
+            let is_vertical: bool = (x == 0 || x == WIDTH-1);
+            let is_diagonal: bool = (x == y || x == HEIGHT-y);
+
+            let is_print: bool = is_horizontal || is_vertical || is_diagonal;
+            if is_print {print!("*")} else {print!(" ")}
+        }
+        println!();
+    }
 }
