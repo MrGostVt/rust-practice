69bc3e2638daf1acef78da7c08f6b50a8c4a2f79
Vitalii <vitalijborisenko27@gmail.com>
2025-04-02 14:15:11 +0300

    Practichna 3, Convert in console. Adaptive UPD

--- src/homework/hw01.rs
+++ src/homework/hw01.rs
@@ -19,14 +19,14 @@ fn main(){
 }
 #[test]
 fn clear_code_competition(){
-    const WIDTH: u32 = 25;
-    const HEIGHT: u32 = 25;
+    const WIDTH: u32 = 32;
+    const HEIGHT: u32 = 22;
 
     for y in 0..HEIGHT {
         for x in 0..WIDTH {
             let is_horizontal: bool = (y == 0 || y == HEIGHT-1);
             let is_vertical: bool = (x == 0 || x == WIDTH-1);
-            let is_diagonal: bool = (x == y || x == HEIGHT-y);
+            let is_diagonal: bool = (x == y*WIDTH/HEIGHT || x == WIDTH - y*WIDTH/HEIGHT-WIDTH/HEIGHT);
 
             let is_print: bool = is_horizontal || is_vertical || is_diagonal;
             if is_print {print!("*")} else {print!(" ")}
